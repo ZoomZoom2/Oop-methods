cee48aafbfd9c72c05c2e47e884103d111de4832
ZoomZoom24 <solversoft@yandex.ru>
2024-03-01 20:30:50 +0300

    Second commit

--- src/js/Characters/Character.js
+++ src/js/Characters/Character.js
@@ -1,45 +1,21 @@
 export default class Character {
     constructor(name, type) {
-        this.name = name;
+        const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
-        if (this.name.length <= 2) {
+        if (name.length <= 2) {
             throw new Error('Имя должно быть длиной более 2 символов');
         }
-        if (this.name.length > 10) {
+        if (name.length > 10) {
             throw new Error('Имя должно быть длиной менее 10 символов');
+        }
+        if (!types.includes(type)) {
+            throw new Error('Нет такого типа персонажа');
         }
 
         this.health = 100;
         this.level = 1;
         this.type = type;
-        switch (this.type) {
-            case 'Bowman':
-                this.attack = 25;
-                this.defence = 25;
-                break;
-            case 'Swordsman':
-                this.attack = 40;
-                this.defence = 10;
-                break;
-            case 'Magician':
-                this.attack = 10;
-                this.defence = 40;
-                break;
-            case 'Daemon':
-                this.attack = 10;
-                this.defence = 40;
-                break;
-            case 'Undead':
-                this.attack = 25;
-                this.defence = 25;
-                break;
-            case 'Zombie':
-                this.attack = 40;
-                this.defence = 10;
-                break;
-            default:
-                throw new Error('Нет такого типа персонажа');
-        }
+        this.name = name;
     }
 
     levelUp() {
